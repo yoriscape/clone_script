b9e882738eb1d6d12ff783be42801483bafdcb04
Yoriscape <36034638+yoriscape@users.noreply.github.com>
2023-10-17 20:57:46 +0530

    Update alioth.sh

--- alioth.sh
+++ alioth.sh
@@ -5,14 +5,11 @@ rm -rf device/xiaomi/alioth
 rm -rf vendor/xiaomi/alioth
 rm -rf device/xiaomi/sm8250-common
 rm -rf vendor/xiaomi/sm8250-common
-rm -rf kernel/xiaomi/sm8250
 
 # Define stuff
 zeph=https://github.com/Project-Zephyrus
-void=
-https://github.com/LineageOS
-git clone -b aosp-14 $zeph/android_device_xiaomi_alioth device/xiaomi/alioth
-git clone -b aosp-14 $zeph/android_device_xiaomi_sm8250-common device/xiaomi/sm8250-common
-git clone -b aosp-14 $zeph/android_vendor_xiaomi_alioth vendor/xiaomi/alioth
-git clone -b aosp-14 $zeph/android_vendor_xiaomi_sm8250-common vendor/xiaomi/sm8250-common
-git clone $void/android_kernel_xiaomi_sm8250 kernel/xiaomi/sm8250
+
+git clone $zeph/android_device_xiaomi_alioth device/xiaomi/alioth
+git clone $zeph/android_device_xiaomi_sm8250-common device/xiaomi/sm8250-common
+git clone $zeph/android_vendor_xiaomi_alioth vendor/xiaomi/alioth
+git clone $zeph/android_vendor_xiaomi_sm8250-common vendor/xiaomi/sm8250-common
